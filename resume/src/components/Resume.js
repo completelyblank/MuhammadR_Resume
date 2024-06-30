81854d8b0befe5cb3b7ed156fb513aa4e148c8e4
Muhammad Raza <105001837+completelyblank@users.noreply.github.com>
2024-06-30 17:26:20 +0500

    Update Resume.js

--- resume/src/components/Resume.js
+++ resume/src/components/Resume.js
@@ -3,26 +3,44 @@ import Typewriter from 'typewriter-effect';
 import { useInView } from 'react-intersection-observer';
 import './styles.css'; // Import your CSS file for additional styling
 
+// Import images
+import muhammad from './muhammad.jpg';
+import skillsIcon from './skills.png';
+import hobbiesIcon from './hobbies.png';
+import multedio from './multedio.png';
+import pixelOdyssey from './pixel_odyssey.jpg';
+import vulner from './vulner.png';
+import animeMern from './anime_mern.png';
+import axnos from './axnos.jpg';
+import terraInc from './terra_inc.png';
+import weather from './weather.png';
+import webDesign from './web_design.png';
+import frontendDev from './frontend_dev.png';
+import javaAlgo from './java_algo.png';
+import dataVis from './data_vis.png';
+import knn from './kNN.webp';
+import pak from './pak.jpg';
+
 const projects = [
-  { id: 1, title: 'Multedio', description: 'Operating Systems Project. Multithreaded improved version of Adobe Lightroom.', bg: 'multedio.png', link: 'https://github.com/completelyblank/Multedio' },
-  { id: 2, title: 'Pixel Odyssey', description: 'Data Structures Project. 2D Pixel Exploration game using DS Concepts.', bg: 'pixel_odyssey.jpg', link: 'https://github.com/completelyblank/Pixel-Odyssey' },
-  { id: 3, title: 'Vulner [Ongoing]', description: 'Cyber Security Project. Flask + Vanilla (JS + HTML + CSS) to showcase Cyber Security Concepts.', bg: 'vulner.png', link: 'https://github.com/completelyblank/Vulner' },
-  { id: 4, title: 'Anime_MERN', description: 'Full Stack CRUD App. MERN Stack with a twist.', bg: 'anime_mern.png', link: 'https://github.com/completelyblank/Anime-MERN' },
-  { id: 5, title: 'Axnos [Abandoned]', description: 'Flutter Game which had to be abandoned because of load on Flutter.', bg: 'axnos.jpg', link: 'https://github.com/completelyblank/Axnos' },
-  { id: 6, title: 'Terra Incognita [Ongoing]', description: 'Pygame Simulation projects using ML Algorithms.', bg: 'terra_inc.png', link: 'https://github.com/completelyblank/Terra-Incognita' },
-  { id: 7, title: 'Blank Weather', description: 'API Caller Project using Open Weather API.', bg: 'weather.png', link: 'https://github.com/completelyblank/Blank-Weather' },
+  { id: 1, title: 'Multedio', description: 'Operating Systems Project. Multithreaded improved version of Adobe Lightroom.', bg: multedio, link: 'https://github.com/completelyblank/Multedio' },
+  { id: 2, title: 'Pixel Odyssey', description: 'Data Structures Project. 2D Pixel Exploration game using DS Concepts.', bg: pixelOdyssey, link: 'https://github.com/completelyblank/Pixel-Odyssey' },
+  { id: 3, title: 'Vulner [Ongoing]', description: 'Cyber Security Project. Flask + Vanilla (JS + HTML + CSS) to showcase Cyber Security Concepts.', bg: vulner, link: 'https://github.com/completelyblank/Vulner' },
+  { id: 4, title: 'Anime_MERN', description: 'Full Stack CRUD App. MERN Stack with a twist.', bg: animeMern, link: 'https://github.com/completelyblank/Anime-MERN' },
+  { id: 5, title: 'Axnos [Abandoned]', description: 'Flutter Game which had to be abandoned because of load on Flutter.', bg: axnos, link: 'https://github.com/completelyblank/Axnos' },
+  { id: 6, title: 'Terra Incognita [Ongoing]', description: 'Pygame Simulation projects using ML Algorithms.', bg: terraInc, link: 'https://github.com/completelyblank/Terra-Incognita' },
+  { id: 7, title: 'Blank Weather', description: 'API Caller Project using Open Weather API.', bg: weather, link: 'https://github.com/completelyblank/Blank-Weather' },
 ];
 
 const certifications = [
-  { id: 1, title: 'Web Design Certification', description: 'freeCodeCamp Web Design.', bg: 'web_design.png', link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
-  { id: 2, title: 'Frontend Development Certification', description: 'freeCodeCamp Frontend Development.', bg: 'frontend_dev.png', link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
-  { id: 3, title: 'Legacy JavaScript Algorithms Certification', description: 'freeCodeCamp JavaScript Development.', bg: 'java_algo.png', link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
-  { id: 4, title: 'Data Visualization Certification', description: 'freeCodeCamp D3 Visualization.', bg: 'data_vis.png', link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
+  { id: 1, title: 'Web Design Certification', description: 'freeCodeCamp Web Design.', bg: webDesign, link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
+  { id: 2, title: 'Frontend Development Certification', description: 'freeCodeCamp Frontend Development.', bg: frontendDev, link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
+  { id: 3, title: 'Legacy JavaScript Algorithms Certification', description: 'freeCodeCamp JavaScript Development.', bg: javaAlgo, link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
+  { id: 4, title: 'Data Visualization Certification', description: 'freeCodeCamp D3 Visualization.', bg: dataVis, link: 'https://www.freecodecamp.org/fcc76c7dae4-cf43-4d34-a299-348d8dd71275' },
 ];
 
 const articles = [
-  { id: 1, title: 'Understanding KNN', description: 'A Medium Article on a theoretical beginning to kNN.', bg: 'kNN.webp', link: 'https://medium.com/@muhammadrkhan272/understanding-knn-0e3d29de5586' },
-  { id: 2, title: 'The WORLD & PAKISTAN: COVID EXAM POLICIES', description: 'An article I wrote for publication at Manzil Pakistan, in Jun 7, 2021.', bg: 'pak.jpg', link: 'https://medium.com/@muhammadrkhan272/the-world-pakistan-covid-exam-policies-0d5e5786d93f' },
+  { id: 1, title: 'Understanding KNN', description: 'A Medium Article on a theoretical beginning to kNN.', bg: knn, link: 'https://medium.com/@muhammadrkhan272/understanding-knn-0e3d29de5586' },
+  { id: 2, title: 'The WORLD & PAKISTAN: COVID EXAM POLICIES', description: 'An article I wrote for publication at Manzil Pakistan, in Jun 7, 2021.', bg: pak, link: 'https://medium.com/@muhammadrkhan272/the-world-pakistan-covid-exam-policies-0d5e5786d93f' },
 ];
 
 const Resume = () => {
@@ -44,17 +62,17 @@ const Resume = () => {
   const { ref: articlesRef, inView: articlesInView } = useInView({ triggerOnce: false, threshold: 0.1 });
 
   return (
-    <div className="p-8 min-h-screen text-white">
+    <div className="p-4 md:p-8 min-h-screen text-white">
       <header className="text-center mb-8">
-        <h1 className={`text-8xl text-gray-500 font-bold floresta transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>Resume</h1>
-        <p className={`text-6xl text-gray-500 mt-4 floresta transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>Muhammad Raza Khan</p>
+        <h1 className={`text-4xl md:text-8xl text-gray-500 font-bold floresta transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>Resume</h1>
+        <p className={`text-2xl md:text-6xl text-gray-500 mt-4 floresta transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>Muhammad Raza Khan</p>
         <img
-          src="muhammad.jpg"
+          src={muhammad}
           alt="Muhammad Raza"
-          className={`w-32 h-32 rounded-full mx-auto mt-4 border-4 border-gray-800 transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}
+          className={`w-16 h-16 md:w-32 md:h-32 rounded-full mx-auto mt-4 border-4 border-gray-800 transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}
           style={{ filter: 'brightness(0.8)' }}
         />
-        <div className={`text-gray-300 text-8x1 mt-4 transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>
+        <div className={`text-gray-300 text-xl md:text-3xl mt-4 transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>
           <Typewriter
             options={{
               strings: [
@@ -72,94 +90,81 @@ const Resume = () => {
         </div>
       </header>
 
-      <section ref={skillsRef} className={`mt-8 glassy-div flex transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>
+      <section ref={skillsRef} className={`mt-8 glassy-div flex flex-col md:flex-row transition-opacity duration-1000 ${skillsInView ? 'opacity-100' : 'opacity-0'}`}>
         <div className="p-4 rounded-lg flex-1 flex items-center">
-          <img src="skills.png" alt="Skills Icon" className="w-64 h-64 mr-4" />
+          <img src={skillsIcon} alt="Skills Icon" className="w-32 h-32 md:w-64 md:h-64 mr-4" />
           <div className="flex-1">
-            <h2 className="text-6xl text-gray-400 section-header floresta text-right">Skills</h2>
+            <h2 className="text-4xl md:text-6xl text-gray-400 section-header floresta text-right">Skills</h2>
             <ul className="list-disc list-inside mt-4 text-gray-500">
               <li>Web Dev (HTML + CSS + Vanilla JS)</li>
               <li>ML (Classification + Training Algorithms) [Beginner]</li>
-              <li>Pixel Art</li>
-              <li>Soundtrack Making</li>
-              <li>Flask as Backend [Beginner]</li>
-              <li>Web Scraping (Selenium + BeautifulSoup)</li>
-              <li>Cyber Security Algorithms [Basics]</li>
-              <li>Operating Systems + Data Structures</li>
-              <li>MERN Stack</li>
-              <li>Data Visualization (D3/Matplotlib)</li>
-              <li>API Calls</li>
+              <li>Cyber Security [Beginner]</li>
+              <li>Full Stack (React + Express + MongoDB + NodeJS)</li>
+              <li>Creative Coding (Pygame + P5.js)</li>
+              <li>Programming (C++ + Python)</li>
             </ul>
           </div>
         </div>
-        <div ref={hobbiesRef} className={`p-4 rounded-lg flex-1 flex items-center transition-opacity duration-1000 ${hobbiesInView ? 'opacity-100' : 'opacity-0'}`}>
+      </section>
+
+      <section ref={hobbiesRef} className={`mt-8 glassy-div flex flex-col md:flex-row transition-opacity duration-1000 ${hobbiesInView ? 'opacity-100' : 'opacity-0'}`}>
+        <div className="p-4 rounded-lg flex-1 flex items-center">
           <div className="flex-1">
-            <h2 className="text-6xl text-gray-400 section-header floresta">Hobbies</h2>
+            <h2 className="text-4xl md:text-6xl text-gray-400 section-header floresta">Hobbies</h2>
             <ul className="list-disc list-inside mt-4 text-gray-500">
-              <li>Dream Analysis</li>
-              <li>Creative Writing</li>
+              <li>Writing</li>
               <li>Gaming</li>
-              <li>Pixel Art</li>
-              <li>Composing Music</li>
-              <li>Exploring Web Development</li>
-              <li>ML Exploration</li>
+              <li>Watching Movies + TV Series + Anime</li>
+              <li>Doing Science</li>
+              <li>History Buff</li>
             </ul>
           </div>
-          <img src="hobbies.png" alt="Hobbies Icon" className="w-64 h-64 ml-4" />
+          <img src={hobbiesIcon} alt="Hobbies Icon" className="w-32 h-32 md:w-64 md:h-64 ml-4" />
         </div>
       </section>
 
-      <section ref={projectsRef} className={`mt-8 glassy-div transition-opacity duration-1000 ${projectsInView ? 'opacity-100' : 'opacity-0'}`}>
-        <h2 className="text-6xl text-gray-400 section-header floresta">Projects</h2>
-        <div className="flex justify-between items-center">
-        <button
-  onClick={prevProject}
-  className="text-gray-200 bg-gray-800 hover:bg-gray-600 hover:text-white text-2xl px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-110"
->
-  &lt;
-</button>
-
-          <div className="w-3/4 p-4 rounded-lg flex items-center">
-            <img src={projects[currentProject].bg} alt={projects[currentProject].title} className="w-64 h-64 mr-4 transition-transform duration-500 transform hover:scale-105" />
-            <div className="flex-1">
-              <h3 className="text-3xl text-gray-400 section-header ">{projects[currentProject].title}</h3>
-              <p className="mt-2 text-gray-500">{projects[currentProject].description}</p>
-              <a href={projects[currentProject].link} target="_blank" rel="noopener noreferrer" className="block mt-2 text-gray-500 underline hover:text-gray-400">View Project</a>
+      <section ref={projectsRef} className={`mt-8 glassy-div flex flex-col items-center transition-opacity duration-1000 ${projectsInView ? 'opacity-100' : 'opacity-0'}`}>
+        <h2 className="text-4xl md:text-6xl text-gray-400 section-header floresta">Projects</h2>
+        <div className="mt-4 flex items-center justify-center">
+          <button onClick={prevProject} className="px-4 py-2 m-2 bg-gray-700 hover:bg-gray-600 text-white rounded">Previous</button>
+          <div className="w-64 h-64 bg-cover bg-center rounded-lg border-4 border-gray-800" style={{ backgroundImage: `url(${projects[currentProject].bg})` }}>
+            <div className="flex flex-col justify-center items-center h-full p-4 bg-black bg-opacity-50 rounded-lg">
+              <h3 className="text-2xl text-gray-100 mb-2">{projects[currentProject].title}</h3>
+              <p className="text-gray-400 mb-4">{projects[currentProject].description}</p>
+              <a href={projects[currentProject].link} target="_blank" rel="noopener noreferrer" className="text-blue-500">View Project</a>
             </div>
           </div>
-          <button onClick={nextProject} className="text-gray-200 bg-gray-800 hover:bg-gray-600 hover:text-white text-2xl px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-110">&gt;</button>
+          <button onClick={nextProject} className="px-4 py-2 m-2 bg-gray-700 hover:bg-gray-600 text-white rounded">Next</button>
         </div>
       </section>
 
-      <section ref={certificationsRef} className={`mt-8 glassy-div transition-opacity duration-1000 ${certificationsInView ? 'opacity-100' : 'opacity-0'}`}>
-        <h2 className="text-6xl text-gray-400 section-header floresta">Certifications</h2>
-        <div className="flex justify-between items-center">
-          <button onClick={prevCertification} className="text-gray-200 bg-gray-800 hover:bg-gray-600 hover:text-white text-2xl px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-110">&lt;</button>
-          <div className="w-3/4 p-4 rounded-lg flex items-center">
-            <img src={certifications[currentCertification].bg} alt={certifications[currentCertification].title} className="w-64 h-64 mr-4 transition-transform duration-500 transform hover:scale-105" />
-            <div className="flex-1">
-              <h3 className="text-3xl text-gray-400 section-header ">{certifications[currentCertification].title}</h3>
-              <p className="mt-2 text-gray-500">{certifications[currentCertification].description}</p>
-              <a href={certifications[currentCertification].link} target="_blank" rel="noopener noreferrer" className="block mt-2 text-gray-500 underline hover:text-gray-400">View Certification</a>
+      <section ref={certificationsRef} className={`mt-8 glassy-div flex flex-col items-center transition-opacity duration-1000 ${certificationsInView ? 'opacity-100' : 'opacity-0'}`}>
+        <h2 className="text-4xl md:text-6xl text-gray-400 section-header floresta">Certifications</h2>
+        <div className="mt-4 flex items-center justify-center">
+          <button onClick={prevCertification} className="px-4 py-2 m-2 bg-gray-700 hover:bg-gray-600 text-white rounded">Previous</button>
+          <div className="w-64 h-64 bg-cover bg-center rounded-lg border-4 border-gray-800" style={{ backgroundImage: `url(${certifications[currentCertification].bg})` }}>
+            <div className="flex flex-col justify-center items-center h-full p-4 bg-black bg-opacity-50 rounded-lg">
+              <h3 className="text-2xl text-gray-100 mb-2">{certifications[currentCertification].title}</h3>
+              <p className="text-gray-400 mb-4">{certifications[currentCertification].description}</p>
+              <a href={certifications[currentCertification].link} target="_blank" rel="noopener noreferrer" className="text-blue-500">View Certification</a>
             </div>
           </div>
-          <button onClick={nextCertification} className="text-gray-200 bg-gray-800 hover:bg-gray-600 hover:text-white text-2xl px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-110">&gt;</button>
+          <button onClick={nextCertification} className="px-4 py-2 m-2 bg-gray-700 hover:bg-gray-600 text-white rounded">Next</button>
         </div>
       </section>
 
-      <section ref={articlesRef} className={`mt-8 glassy-div transition-opacity duration-1000 ${articlesInView ? 'opacity-100' : 'opacity-0'}`}>
-        <h2 className="text-6xl text-gray-400 section-header floresta">Articles</h2>
-        <div className="flex justify-between items-center">
-          <button onClick={prevArticle} className="text-gray-200 bg-gray-800 hover:bg-gray-600 hover:text-white text-2xl px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-110">&lt;</button>
-          <div className="w-3/4 p-4 rounded-lg flex items-center">
-            <img src={articles[currentArticle].bg} alt={articles[currentArticle].title} className="w-64 h-64 mr-4 transition-transform duration-500 transform hover:scale-105" />
-            <div className="flex-1">
-              <h3 className="text-3xl text-gray-400 section-header">{articles[currentArticle].title}</h3>
-              <p className="mt-2 text-gray-500">{articles[currentArticle].description}</p>
-              <a href={articles[currentArticle].link} target="_blank" rel="noopener noreferrer" className="block mt-2 text-gray-500 underline hover:text-gray-400">View Article</a>
+      <section ref={articlesRef} className={`mt-8 glassy-div flex flex-col items-center transition-opacity duration-1000 ${articlesInView ? 'opacity-100' : 'opacity-0'}`}>
+        <h2 className="text-4xl md:text-6xl text-gray-400 section-header floresta">Articles</h2>
+        <div className="mt-4 flex items-center justify-center">
+          <button onClick={prevArticle} className="px-4 py-2 m-2 bg-gray-700 hover:bg-gray-600 text-white rounded">Previous</button>
+          <div className="w-64 h-64 bg-cover bg-center rounded-lg border-4 border-gray-800" style={{ backgroundImage: `url(${articles[currentArticle].bg})` }}>
+            <div className="flex flex-col justify-center items-center h-full p-4 bg-black bg-opacity-50 rounded-lg">
+              <h3 className="text-2xl text-gray-100 mb-2">{articles[currentArticle].title}</h3>
+              <p className="text-gray-400 mb-4">{articles[currentArticle].description}</p>
+              <a href={articles[currentArticle].link} target="_blank" rel="noopener noreferrer" className="text-blue-500">Read Article</a>
             </div>
           </div>
-          <button onClick={nextArticle} className="text-gray-200 bg-gray-800 hover:bg-gray-600 hover:text-white text-2xl px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-110">&gt;</button>
+          <button onClick={nextArticle} className="px-4 py-2 m-2 bg-gray-700 hover:bg-gray-600 text-white rounded">Next</button>
         </div>
       </section>
     </div>
